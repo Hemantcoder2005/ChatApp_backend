5483fd06cc15f8da0ae41c6ecd16ab10e2205537
Hemantcoder2005 <hemantnarula2203@outlook.com>
2025-04-05 14:21:28 +0530

    cronJobsetup

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-
+from .views import WarmMe
 urlpatterns = [
    path("accounts/",include("accounts.urls")),
-   path("friends/",include("friends_handler.urls"))
+   path("friends/",include("friends_handler.urls")),
+   path("warmme/", WarmMe)
 ]
